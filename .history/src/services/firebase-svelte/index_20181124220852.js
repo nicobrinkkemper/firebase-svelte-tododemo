a67ed9d230db49764ec0ce91efbad9e2e9763680
nicobrinkkemper <nico@brinkkemper.nl>
2018-11-25 01:12:38 +0100

    basic functionality

--- .history/src/services/firebase-svelte/index_20181124220852.js
+++
@@ -1,7 +0,0 @@
-import { Store as SvelteStore } from 'svelte/store.js';
-import stringHash from 'string-hash'
-export class Store extends SvelteStore {
-    
-}
-
-export default Store;
--- .history/src/services/firebase-svelte/index_20181124221559.js
+++
@@ -1,7 +0,0 @@
-import { Store as SvelteStore } from 'svelte/store.js';
-
-export class Store extends SvelteStore {
-    
-}
-
-export default Store;
